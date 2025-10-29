97a387e419dcf19d230644b22bca0f5cd71f291a
juliaherdy <juliaherdymoura@gmail.com>
2025-10-29 11:48:12 -0300

    structure running in a single file

--- index.php
+++ index.php
@@ -5,6 +5,24 @@ if (! $conn) {
     die("Erro ao se conectar no banco de dados");
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $task_title = trim($_POST["task_title"]);
+    $task_description = trim($_POST["task_description"]);
+    $deadline = trim($_POST["deadline"]);
+    if ($task_title !== "") {
+        $query = "insert into tasks (task_title, task_description, deadline) values ($1, $2, $3)";
+        $result = pg_query_params($conn, $query, [$task_title, $task_description, $deadline]);
+
+        if ($result) {
+            echo "Task cadastrada com sucesso!";
+        } else {
+            echo "Erro ao cadastrar a task: " . pg_last_error($conn);
+        }
+    } else {
+        echo "Task é Obrigatória!";
+    }
+}
+
 if (isset($_GET["delete"])) {
     $id = (int)$_GET["delete"];
     $query = "delete from tasks where id = $1";
@@ -36,72 +54,77 @@ $tasks = pg_fetch_all($tasklist);
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>TO DO LIST</h1>
-    <form method="POST" action="?action=tasks">
-        <label for="client">Atividade:</label>
-        <input type="text" id="amount" name="amount" required>
+    <div class="inputs">
+        <h1>TO DO LIST</h1>
+        <form method="POST" action="?action=tasks">
+            <label for="task_title">Task:</label>
+            <input type="text" id="task_title" name="task_title" required>
 
-        <label for="amount">Descrição:</label>
-        <input type="text" id="amount" name="amount" required>
+            <label for="task_description">Descrição:</label>
+            <input type="text" id="task_description" name="task_description" required>
 
-        <label for="amount">Prazo:</label>
-        <input type="date" id="date" name="amount" required>
+            <label for="deadline">Prazo:</label>
+            <input type="date" id="deadline" name="deadline" required>
 
-        <button type="submit">Salvar</button>
-    </form>
+            <button type="submit">Salvar</button>
+        </form>
+    </div>
 
 
-    <?php if (!empty($tasks)) { ?>
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>Task</th>
-                <th>Descrição</th>
-                <th>Data de início</th>
-                <th>Prazo</th>
-                <th>Ações</th>
-            </tr>
-            <?php foreach ($tasks as $row) {
-                     if ($row["status"] == 1) {?>
+    <div class="taskstable">
+        <?php if (!empty($tasks)) { ?>
+            <table>
                 <tr>
-                    <td><?= $row['id'] ?></td>
-                    <td class="overline"><?= $row['task_title'] ?></td>
-                    <td class="overline"><?= $row['task_description'] ?></td>
-                    <td><?= $row['start_date'] ?></td>
-                    <td><?= $row['deadline'] ?></td>
-                    <td>
-                        <a class="delete-btn" href="?delete=<?= urlencode($row['id'] ?? '') ?>" onclick="return confirm('Tem certeza que deseja excluir esta tarefa?');">
-                            Excluir
-                        </a>
-                        <br>
-                        <br>
-                        <a class="complete-btn" href="?changestatus=<?= urlencode($row['id'] ?? '') ?>">
-                            Feito
-                        </a>
-                    </td>
+                    <th>Task</th>
+                    <th>Data de início</th>
+                    <th>Prazo</th>
+                    <th>Ações</th>
                 </tr>
-            <?php } ?>
-                <tr>
-                    <td><?= $row['id'] ?></td>
-                    <td><?= $row['task_title'] ?></td>
-                    <td><?= $row['task_description'] ?></td>
-                    <td><?= $row['start_date'] ?></td>
-                    <td><?= $row['deadline'] ?></td>
-                    <td>
-                        <a class="delete-btn" href="?delete=<?= urlencode($row['id'] ?? '') ?>" onclick="return confirm('Tem certeza que deseja excluir esta tarefa?');">
-                            Excluir
-                        </a>
-                        <br>
-                        <br>
-                        <a class="complete-btn" href="?changestatus=<?= urlencode($row['id'] ?? '') ?>">
-                            Feito
-                        </a>
-                    </td>
-                </tr>
-            <?php } ?>
-        </table>
-    <?php } else { ?>
-        <p>Nenhum Pedido cadastrado.</p>
-    <?php } ?>
+                <?php foreach ($tasks as $row) {
+                    if ($row["status"] == 1) {?>
+                        <tr>
+                            <td class="overline">
+                                <h2 class="title"><?= $row['task_title'] ?></h2>
+                                <p class="description"><?= $row['task_description'] ?></p>
+                            </td>
+                            <td><?= $row['start_date'] ?></td>
+                            <td><?= $row['deadline'] ?></td>
+                            <td>
+                                <a class="delete-btn" href="?delete=<?= urlencode($row['id'] ?? '') ?>" onclick="return confirm('Tem certeza que deseja excluir esta tarefa?');">
+                                    Excluir
+                                </a>
+                                <br>
+                                <br>
+                                <a class="complete-btn" href="?changestatus=<?= urlencode($row['id'] ?? '') ?>">
+                                    Feito
+                                </a>
+                            </td>
+                        </tr>
+                    <?php } else {?>
+                    <tr>
+                        <td>
+                            <h2 class="title"><?= $row['task_title'] ?></h2>
+                            <p class="description"><?= $row['task_description'] ?></p>
+                        </td>
+                        <td><?= $row['start_date'] ?></td>
+                        <td><?= $row['deadline'] ?></td>
+                        <td>
+                            <a class="delete-btn" href="?delete=<?= urlencode($row['id'] ?? '') ?>" onclick="return confirm('Tem certeza que deseja excluir esta tarefa?');">
+                                Excluir
+                            </a>
+                            <br>
+                            <br>
+                            <a class="complete-btn" href="?changestatus=<?= urlencode($row['id'] ?? '') ?>">
+                                Feito
+                            </a>
+                        </td>
+                    </tr>
+                <?php } } ?>
+            </table>
+        <?php } else { ?>
+            <p>Nenhum Pedido cadastrado.</p>
+        <?php } ?>
+    </div>
+
 
 </body>
